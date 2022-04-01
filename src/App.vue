c0fc18acc631ec9a057929990272fe5adb138915
twotimesgi <luigibardellagerbi@mail.com>
2022-04-01 13:47:24 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header-comp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp
   }
 }
 </script>
